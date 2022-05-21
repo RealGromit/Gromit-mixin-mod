c415f67ecbf6f25d5c14eaac12d283421abf918e
LiamPC <87600704+JoeV6@users.noreply.github.com>
2022-05-21 23:39:35 +0200

    Add files via upload

--- src/main/java/com/gromit/gromitmod/gui/module/FunModuleGui.java
+++ src/main/java/com/gromit/gromitmod/gui/module/FunModuleGui.java
@@ -2,12 +2,46 @@ package com.gromit.gromitmod.gui.module;
 
 import com.gromit.gromitmod.GromitMod;
 import com.gromit.gromitmod.gui.MainGui;
+import com.gromit.gromitmod.gui.button.CheckboxButton;
+import com.gromit.gromitmod.gui.button.TextButton;
+import com.gromit.gromitmod.gui.module.fun.AutoTickGui;
+import com.gromit.gromitmod.gui.module.fun.DebugBlockGui;
 import com.gromit.gromitmod.handler.Saver;
+import com.gromit.gromitmod.module.fun.AutoTick;
+import com.gromit.gromitmod.module.fun.DebugBlock;
 import com.gromit.gromitmod.utils.RenderUtils;
+
+import java.io.IOException;
 
 public class FunModuleGui extends MainGui {
 
     private static FunModuleGui instance;
+
+    protected static final TextButton autoTickButton = new TextButton(101,4, "AutoTick",
+            (button) -> minecraft.displayGuiScreen(AutoTickGui.getInstance()),
+            (button) -> minecraft.displayGuiScreen(FunModuleGui.getInstance()));
+
+    protected static final TextButton debugButton = new TextButton(102,4, "Debug Block",
+            (button) -> minecraft.displayGuiScreen(DebugBlockGui.getInstance()),
+            (button) -> minecraft.displayGuiScreen(FunModuleGui.getInstance()));
+
+    public static final CheckboxButton checkbox1 = new CheckboxButton(103, 4, 4,
+            (button) -> {
+                AutoTick.getInstance().register();
+            },
+
+            (button) -> {
+                AutoTick.getInstance().unregister();
+            });
+
+    public static final CheckboxButton checkbox2 = new CheckboxButton(104, 4, 4,
+            (button) -> {
+                DebugBlock.getInstance().register();
+            },
+
+            (button) -> {
+                DebugBlock.getInstance().unregister();
+            });
 
     public FunModuleGui(GromitMod gromitMod) {
         super(gromitMod);
@@ -22,13 +56,38 @@ public class FunModuleGui extends MainGui {
         crumbs.setState(false);
         fps.setState(false);
         render.setState(false);
+
+        autoTickButton.updateButton(mainGuiPointX + 7, mainGuiPointY + 39, guiScale);
+        checkbox1.updateButton(mainGuiPointX + 49, mainGuiPointY + 39, guiScale);
+        debugButton.updateButton(mainGuiPointX + 7, mainGuiPointY + 46, guiScale);
+        checkbox2.updateButton(mainGuiPointX + 49, mainGuiPointY + 46, guiScale);
+        buttonList.add(autoTickButton);
+        buttonList.add(checkbox1);
+        buttonList.add(debugButton);
+        buttonList.add(checkbox2);
+
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
 
+        autoTickButton.drawButton(minecraft, mouseX, mouseY);
+        checkbox1.drawButton(minecraft, mouseX, mouseY);
+        debugButton.drawButton(minecraft, mouseX, mouseY);
+        checkbox2.drawButton(minecraft, mouseX, mouseY);
+
         RenderUtils.drawLine(mainGuiPointX + 60, mainGuiPointY + 37, 0, 101, 4, 255, 255, 255, 255);
+    }
+
+    @Override
+    public void onGuiClosed() {
+
+    }
+
+    @Override
+    public void handleMouseInput() throws IOException {
+        super.handleMouseInput();
     }
 
     @Override
